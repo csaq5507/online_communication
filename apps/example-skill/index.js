cdd823eb5e815bc72d89359cb47769a3379446da
Ivan Waldboth <Ivan.Waldboth@student.uibk.ac.at>
2018-04-24 16:41:13 +0200

    first real test

--- apps/example-skill/index.js
+++ apps/example-skill/index.js
@@ -3,6 +3,8 @@ module.change_code = 1;
 
 var alexa = require( 'alexa-app' );
 var app = new alexa.app( 'example-skill' );
+var AmazonSpeech = require('ssml-builder/amazon_speech');
+ 
 
 
 app.launch( function( request, response ) {
@@ -17,16 +19,52 @@ app.error = function( exception, request, response ) {
 	response.say( 'Sorry an error occured ' + error.message);
 };
 
-app.intent('say_number',
+app.intent('course_number',
   {
-    "slots":{"NUMBER":"AMAZON.NUMBER"}
+    "slots":{"COURSE":"COURSE"}
 	,"utterances":[ 
-		"say the number {NUMBER}"]
+                        "tell me the number of {COURSE}",
+                        "what's the number of {COURSE}"
+                  ]
   },
   function(request,response) {
-    var number = request.slot('NUMBER');
-    response.say("You asked for the number "+number);
+    var course = request.slot('COURSE');
+    
+    var speech = new AmazonSpeech()
+	  .say('Hi')
+	  .pause('1s')
+	  .whisper('the number you want is')
+	  .pause('500ms')
+	  .say(course.value);
+	 
+	var speechOutput = speech.ssml();
+	response.say(speechOutput);
   }
 );
+
+app.customSlot('COURSE',[
+                        {
+                            id: "dsl",
+                            value: "PS Domain Specific Language",
+                            synonyms: [
+                                    "domain specific language",
+                                    "DSL"
+                                ]
+                        },
+                        {
+							value: "PS Compiler Construction",
+                            synonyms: [
+                                    "compiler construction"
+                                ]
+                        },
+                        {
+                            id: "OC",
+							value: "SL Online Communication",
+                            synonyms: [
+                                   "oc seminar",
+                                    "online communication"
+                                ]
+                        }
+                    ]);
 
 module.exports = app;

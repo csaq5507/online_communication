cc40c291ce191d96fd3a05c3308e9e3ee04d8966
Ivan Waldboth <Ivan.Waldboth@student.uibk.ac.at>
2018-05-02 14:57:45 +0200

    test on real alexa server

--- apps/example-skill/index.js
+++ apps/example-skill/index.js
@@ -32,7 +32,7 @@ app.intent('course_number', {
         ]
     },
     function(request, response) {
-        var slot = (slot?slot:request.slots.COURSE);
+    //    var slot = (slot?slot:request.slots.COURSE);
         console.log(slot.resolutions);
         var resolution = (slot.resolutions && slot.resolutions.resolutionsPerAuthority && slot.resolutions.resolutionsPerAuthority.length > 0) ? slot.resolutions.resolutionsPerAuthority[0] : null;
         if(resolution && resolution.status.code == 'ER_SUCCESS_MATCH'){
